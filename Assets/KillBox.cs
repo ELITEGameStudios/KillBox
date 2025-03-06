d7d7122b56c5c6c4b916efa4e000e7918cd45ea3
ELITEGameStudios <noahxcmi@gmail.com>
2025-03-06 15:38:33 -0500

    Created universal script and core scene
    This is the commit before I tamper with the GameManager class and mess a lot of things up

---
+++ Assets/KillBox.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    // The universal game manager across separate games
+    // Should never be destroyed
+
+    void Awake(){
+        
+    }
+}
